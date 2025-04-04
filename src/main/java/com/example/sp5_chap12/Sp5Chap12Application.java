fdfca7532b66987306ae6f49219bd88139548ebc
SYU <xeungjin@gmail.com>
2025-04-04 12:06:42 +0900

    Initial commit

---
+++ src/main/java/com/example/sp5_chap12/Sp5Chap12Application.java
@@ -0,0 +1,13 @@
+package com.example.sp5_chap12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp5Chap12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp5Chap12Application.class, args);
+	}
+
+}
